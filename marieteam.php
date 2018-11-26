01d5d039c7b8222c7ad4e7c46b5d4afa2fc4b3bb
Coussemaeker <43337041+RealCptWolf@users.noreply.github.com>
2018-11-26 11:40:03 +0100

    grosse maj

---
+++ marieteam.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+
+echo "Connected successfully";
+?>
--- registration.php
+++ registration.php
@@ -7,37 +7,39 @@
 </head>
 <body>
 <?php
-require('db.php');
+require('marieteam.php');
 // If form submitted, insert values into the database.
 if (isset($_REQUEST['nom'])){
         // removes backslashes
 	$nom = stripslashes($_REQUEST['nom']);
         //escapes special characters in a string
-	$nom = mysqli_real_escape_string($con,$nom); 
+	$nom = mysqli_real_escape_string($nom); 
+	$prenom = stripslashes($_REQUEST['prenom']);
+	$prenom = mysqli_real_escape_string($prenom); 
 	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
+	$email = mysqli_real_escape_string($email);
 	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users` (nom, password, email, trn_date)
-VALUES ('$nom', '".md5($password)."', '$email', '$trn_date')";
-        $result = mysqli_query($con,$query);
+	$password = mysqli_real_escape_string($password);
+        $query = "INSERT into `utilisateur` (nom, prenom, password, email)
+VALUES ('$nom', '".md5($password)."', '$email')";
+        $result = mysqli_query($query);
         if($result){
             echo "<div class='form'>
-<h3>You are registered successfully.</h3>
-<br/>Click here to <a href='signin.php'>Sign in</a></div>";
+<h3>Vous êtes bien enregistré(e).</h3>
+<br/>Cliquez ici pour vous <a href='signin.php'>necter</a></div>";
         }
     }else{
 ?>
 <div class="form">
-<h1>Registration</h1>
+<h1>Inscription</h1>
 <form name="registration" action="" method="post">
 <input type="text" name="nom" placeholder="nom" required />
+<input type="text" name="prenom" placeholder="prenom" required />
 <input type="email" name="email" placeholder="email" required />
 <input type="password" name="password" placeholder="Password" required />
 <input type="submit" name="submit" value="Register" />
 </form>
-<p>Already registered? <a href='signin.php'>Sign in here</a></p>
+<p>Déjà enregistré(e)? <a href='signin.php'>Connectez vous ici</a></p>
 </div>
 <?php } ?>
 </body>
