09372c087dfed6602b0e65d6d97211b35327dfed
Coussemaeker <43337041+RealCptWolf@users.noreply.github.com>
2018-11-26 10:59:53 +0100

    modification

--- signin.php
+++ signin.php
@@ -10,25 +10,25 @@
 require('db.php');
 session_start();
 // If form submitted, insert values into the database.
-if (isset($_POST['username'])){
+if (isset($_POST['nom'])){
         // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
+	$nom = stripslashes($_REQUEST['nom']);
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username);
+	$nom = mysqli_real_escape_string($con,$nom);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
+        $query = "SELECT * FROM `users` WHERE nom='$nom'
 and password='".md5($password)."'";
 	$result = mysqli_query($con,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
         if($rows==1){
-	    $_SESSION['username'] = $username;
+	    $_SESSION['nom'] = $nom;
             // Redirect user to index.php
 	    header("Location: index.php");
          }else{
 	echo "<div class='form'>
-<h3>Username/password is incorrect.</h3>
+<h3>nom/password is incorrect.</h3>
 <br/>Click here to <a href='signin.php'>Sign in</a></div>";
 	}
     }else{
@@ -36,7 +36,7 @@ and password='".md5($password)."'";
 <div class="form">
 <h1>Log In</h1>
 <form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
+<input type="text" name="nom" placeholder="nom" required />
 <input type="password" name="password" placeholder="Password" required />
 <input name="submit" type="submit" value="Login" />
 </form>
