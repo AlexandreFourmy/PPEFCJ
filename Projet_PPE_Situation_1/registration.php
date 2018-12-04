e30769d2d555e569894740b77f7def1653c38851
Coussemaeker <43337041+RealCptWolf@users.noreply.github.com>
2018-12-04 11:28:29 +0100

    Update registration.php

--- Projet_PPE_Situation_1/registration.php
+++ Projet_PPE_Situation_1/registration.php
@@ -9,14 +9,15 @@
 <?php
 if (isset($_REQUEST['nom'])){
     $db=new PDO('mysql:host=localhost; dbname=marieteam','root','');
+		$id=000;
 		$username=$_POST['username'];
 		$nom=$_POST['nom'];
 		$prenom=$_POST['prenom'];
 		$email=$_POST['email'];
 		$password=$_POST['password'];
 		
-		$sql=$db->prepare("INSERT INTO utilisateur(username, nom, prenom, email, password) VALUES(?,?,?,?,?)");
-		$sql->execute([$username, $nom, $prenom, $email, $password]);
+		$sql=$db->prepare("INSERT INTO utilisateur(id, username, nom, prenom, email, password) VALUES(?,?,?,?,?)");
+		$sql->execute([$id, $username, $nom, $prenom, $email, $password]);
 		
 		echo "<script type='text/javascript'>";
 		echo "alert('Vous avez bien été enregistré(e)');";
@@ -28,11 +29,11 @@ if (isset($_REQUEST['nom'])){
 <div class="form">
 <h1 id="title">Inscription</h1>
 <form id="form" name="registration" action="" method="post">
-<input class="boxes" type="text" name="username" placeholder="username" required/>
-<input class="boxes" type="text" name="nom" placeholder="nom" required />
-<input class="boxes" type="text" name="prenom" placeholder="prenom" required />
-<input class="boxes" type="email" name="email" placeholder="email" required />
-<input class="boxes" type="password" name="password" placeholder="password" required />
+<input id="username" class="boxes" type="text" name="username" placeholder="username" required/>
+<input id="nom" class="boxes" type="text" name="nom" placeholder="nom" required />
+<input id="prenom" class="boxes" type="text" name="prenom" placeholder="prenom" required />
+<input id="email" class="boxes" type="email" name="email" placeholder="email" required />
+<input id="password" class="boxes" type="password" name="password" placeholder="password" required />
 <input id="submit" type="submit" name="submit" value="S'enregistrer" />
 </form>
 <p>Déjà enregistré(e)? <a href='signin.php'>Connectez vous ici</a></p>
