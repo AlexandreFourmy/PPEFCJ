04df172090e9eead13c06ee7b3a2878edab6d736
Coussemaeker <43337041+RealCptWolf@users.noreply.github.com>
2018-12-04 16:49:32 +0100

    Update signin.php

--- Projet_PPE_Situation_1/signin.php
+++ Projet_PPE_Situation_1/signin.php
@@ -10,34 +10,38 @@
 require('marieteam.php');
 session_start();
 // If form submitted, insert values into the database.
-if (isset($_POST['nom'])){
-        // removes backslashes
-	$nom = stripslashes($_REQUEST['nom']);
+if (isset($_POST['username'])){
+    $sql=$db->prepare("SELECT * FROM utilisateur WHERE username='?'");
+	$sql->execute($_POST['username']);
+	
+		/*TEST COOL
+		// removes backslashes
+	$username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-	$nom = mysqli_real_escape_string($conn,$nom);
+	$nom = mysqli_real_escape_string($conn,$username);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($conn,$password);
 	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE nom='$nom'
+        $query = "SELECT * FROM `users` WHERE username='$username'
 and password='".md5($password)."'";
 	$result = mysqli_query($conn,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
         if($rows==1){
-	    $_SESSION['nom'] = $nom;
+	    $_SESSION['username'] = $username;
             // Redirect user to index.php
 	    header("Location: index.php");
          }else{
 	echo "<div class='form'>
-<h3>nom/password is incorrect.</h3>
-<br/>Click here to <a href='signin.php'>Sign in</a></div>";
-	}
+<h3>username ou mot de passe incorrect.</h3>
+<br/>Cliquez ici pour <a href='signin.php'>vous enregistrer</a></div>";
+	}*/
     }else{
 ?>
 <div class="form">
 <h1 id="title">Se connecter</h1>
 <form action="" method="post" name="login">
-<input class="boxes" type="text" name="nom" placeholder="nom" required />
-<input class="boxes" type="password" name="password" placeholder="Password" required />
+<input id="username" class="boxes" type="text" name="username" placeholder="username" required />
+<input id="password" class="boxes" type="password" name="password" placeholder="password" required />
 <input id="submit" name="submit" type="submit" value="Login" />
 </form>
 <p>Pas encore enregisté? <a href='registration.php'>S'enregistrer ici</a></p>
