b092078d0fe3eef80ecdb16d94fbba4923a1e70c
Coussemaeker <43337041+RealCptWolf@users.noreply.github.com>
2018-12-10 11:46:33 +0100

    refonte indentation + maj script connexion

--- Projet_PPE_Situation_1/registration.php
+++ Projet_PPE_Situation_1/registration.php
@@ -1,43 +1,47 @@
 ﻿<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Registration</title>
-<link rel="stylesheet" href="css/styleLogin.css" />
-</head>
-<body>
-<?php
-if (isset($_REQUEST['nom'])){
-    $db=new PDO('mysql:host=localhost; dbname=marieteam','root','');
-	$username=$_POST['username'];
-	$nom=$_POST['nom'];
-	$prenom=$_POST['prenom'];
-	$email=$_POST['email'];
-	$password=$_POST['password'];
+	<head>
+		<meta charset="utf-8">
+		<title>Registration</title>
+		<link rel="stylesheet" href="css/styleLogin.css" />
+	</head>
+	<body>
+		<?php
+		if (isset($_REQUEST['nom']))
+		{
+			$db=new PDO('mysql:host=localhost; dbname=marieteam','root','');
+			$username=$_POST['username'];
+			$nom=$_POST['nom'];
+			$prenom=$_POST['prenom'];
+			$email=$_POST['email'];
+			$password=$_POST['password'];
 		
-	$sql=$db->prepare("INSERT INTO utilisateur(username, nom, prenom, email, password) VALUES(?,?,?,?,?)");
-	$sql->execute([$username, $nom, $prenom, $email, $password]);
+			$sql=$db->prepare("INSERT INTO utilisateur(username, nom, prenom, email, password) VALUES(?,?,?,?,?)");
+			$sql->execute([$username, $nom, $prenom, $email, $password]);
 		
-	echo "<script type='text/javascript'>";
-	echo "alert('Vous avez bien été enregistré(e)');";
-	echo "window.location.href='index.html';";
-	echo "</script>";
-}
-	else{
-?>
-<div class="form">
-<h1 id="title">Inscription</h1>
-<form id="form" name="registration" action="" method="post">
-<input id="username" class="boxes" type="text" name="username" placeholder="username" required/>
-<input id="nom" class="boxes" type="text" name="nom" placeholder="nom" required />
-<input id="prenom" class="boxes" type="text" name="prenom" placeholder="prenom" required />
-<input id="email" class="boxes" type="email" name="email" placeholder="email" required />
-<input id="password" class="boxes" type="password" name="password" placeholder="password" required />
-<input id="submit" type="submit" name="submit" value="S'enregistrer" />
-</form>
-<p>Déjà enregistré(e)? <a href='signin.php'>Connectez vous ici</a></p>
-<p>Retourner sur le site <a href='index.html'>ici</a></p>
-</div>
-<?php }?>
-</body>
+			echo "<script type='text/javascript'>";
+			echo "alert('Vous avez bien été enregistré(e)');";
+			echo "window.location.href='index.html';";
+			echo "</script>";
+		}
+		else
+		{
+			?>
+			<div class="form">
+				<h1 id="title">Inscription</h1>
+				<form id="form" name="registration" action="" method="post">
+					<input id="username" class="boxes" type="text" name="username" placeholder="username" required/>
+					<input id="nom" class="boxes" type="text" name="nom" placeholder="nom" required />
+					<input id="prenom" class="boxes" type="text" name="prenom" placeholder="prenom" required />
+					<input id="email" class="boxes" type="email" name="email" placeholder="email" required />
+					<input id="password" class="boxes" type="password" name="password" placeholder="password" required />
+					<input id="submit" type="submit" name="submit" value="S'enregistrer" />
+				</form>
+				<p>Déjà enregistré(e)? <a href='signin.php'>Connectez vous ici</a></p>
+				<p>Retourner sur le site <a href='index.html'>ici</a></p>
+			</div>
+			<?php
+		}
+		?>
+	</body>
 </html>
--- Projet_PPE_Situation_1/signin.php
+++ Projet_PPE_Situation_1/signin.php
@@ -1,93 +1,97 @@
 ﻿<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Connexion</title>
-<link rel="stylesheet" href="css/styleLogin.css" />
-</head>
-<body>
-<?php
-if (isset($_POST['username'])){
-    /*	Test #1 connection
-	$sql=$db->prepare("SELECT * FROM utilisateur WHERE username='?'");
-	$sql->execute($_POST['username']);
-	*/
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion</title>
+		<link rel="stylesheet" href="css/styleLogin.css" />
+	</head>
+	<body>
+		<?php
+		/*	Test #1 connection
+		$sql=$db->prepare("SELECT * FROM utilisateur WHERE username='?'");
+		$sql->execute($_POST['username']);
+		*/
 	
 	
-	/*	Test #2 connection
-	$username=$_POST['username'];
-	$password=md5($_POST['password']);
-	try {
-		$dbh = new PDO('mysql:host=localhost;dbname=marieteam','root','');
-		foreach($dbh->query('SELECT * from utilisateur') as $row) {
-        print_r($row);
+		/*	Test #2 connection
+		$username=$_POST['username'];
+		$password=md5($_POST['password']);
+		try {
+			$dbh = new PDO('mysql:host=localhost;dbname=marieteam','root','');
+			foreach($dbh->query('SELECT * from utilisateur') as $row) {
+			print_r($row);
+			}
+			$dbh = null;
 		}
+		catch (PDOException $e) {
+			print "Erreur !: " . $e->getMessage() . "<br/>";
+			die();
+		}
+		$sth = null;
 		$dbh = null;
-	}
-	catch (PDOException $e) {
-		print "Erreur !: " . $e->getMessage() . "<br/>";
-		die();
-	}
-	$sth = null;
-	$dbh = null;
-	*/
-try
-{
-    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-    $db=new PDO('mysql:host=localhost; dbname=marieteam','root','');
-}
-catch(PDOException $e)
-{
-    trigger_error(sprintf('Erreur MySQL %d<br />%s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
-}
- 
-if($_SERVER['REQUEST_METHOD'] == 'POST') // alternative de ton isset($_POST['submit'])
-{
-    $username = filter_input(INPUT_POST, 'username', FILTER_VALIDATE_EMAIL);
-    // $email vaut false si pas bon ou vide, et vaut l'e-mail audrement
-    $password = empty($_POST['password']) ? '' : md5($_POST['password']);
-    // $password vaut '' si inexistant ou chaîne vide, et le md5 sinon
-    if ($username and !empty($password))
-    {
-        try
-        {
-            $query = '	SELECT COUNT(id) AS nb
-							FROM utilisateur
-								WHERE username = :username
-									AND password = :password';
-            $reponse = $db->prepare($query);
-            $reponse->bindValue(':username', $username, PDO::PARAM_STR);
-            $reponse->bindValue(':password', $password, PDO::PARAM_STR);
-            $reponse->execute();
-            $reponse->bindColumn('nb', $nb_ligne, PDO::PARAM_INT);
-            $reponse->fetch(PDO::FETCH_BOUND);
-            if ($nb_ligne > 0)
-                echo "connection ok";
-            else
-                $erreur_post = "1";
-        }
-        catch(PDOException $e)
-        {
-             trigger_error(sprintf('Erreur MySQL %d<br />%s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
-        }
-    }
-}
-}
-else
-{
-	?>
-	<div class="form">
-	<h1 id="title">Se connecter</h1>
-	<form action="" method="post" name="login">
-	<input id="username" class="boxes" type="text" name="username" placeholder="username" required />
-	<input id="password" class="boxes" type="password" name="password" placeholder="password" required />
-	<input id="submit" name="submit" type="submit" value="Login" />
-	</form>
-	<p>Pas encore enregistré(e)? <a href='registration.php'>S'enregistrer ici</a></p>
-	<p>Retourner sur le site <a href='index.html'>ici</a></p>
-	</div>
-	<?php
-} 
-?>
-</body>
+		*/
+		try
+		{
+			$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
+			$connexion = new PDO(sprintf('mysql:host=%s; dbname=%s','localhost','marieteam'),'root','', $options);
+		}
+		catch(PDOException $e)
+		{
+			trigger_error(sprintf('Erreur MySQL %d<br />%s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
+		}
+		if($_SERVER['REQUEST_METHOD'] == 'POST') // alternative du isset($_POST['submit'])
+		{
+			$username = $_POST['username'];
+				// $username vaut false si pas bon ou vide, et vaut l'username audrement
+			$password = empty($_POST['password']) ? '' : md5($_POST['password']);
+				// $password vaut '' si inexistant ou chaîne vide, et le md5 sinon
+			if ($username and !empty($password))
+			{
+				try
+				{
+					$query = '	SELECT COUNT(id) AS nb
+									FROM utilisateur
+										WHERE username = :username
+											AND password = :password';
+					$reponse = $connexion->prepare($query);
+					$reponse->bindValue(':username', $username, PDO::PARAM_STR);
+					$reponse->bindValue(':password', $password, PDO::PARAM_STR);
+					$reponse->execute();
+					$reponse->bindColumn('nb', $nb_ligne, PDO::PARAM_INT);
+					$reponse->fetch(PDO::FETCH_BOUND);
+					if ($nb_ligne > 0)
+					{
+						echo "<script type='text/javascript'>";
+						echo "alert('Vous êtes bien connecté(e)');";
+						echo "window.location.href='index.html';";
+						echo "</script>";
+					}
+					else
+					{
+						$erreur_post = "1";
+					}
+				}
+				catch(PDOException $e)
+				{
+					trigger_error(sprintf('Erreur MySQL %d<br />%s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
+				}
+			}
+		}
+		else
+		{
+			?>
+			<div class="form">
+				<h1 id="title">Se connecter</h1>
+				<form id="form" action="" method="post" name="login">
+					<input id="username" class="boxes" type="text" name="username" placeholder="Nom d'utilisateur" required />
+					<input id="password" class="boxes" type="password" name="password" placeholder="Mot de passe" required />
+					<input id="submit" name="submit" type="submit" value="Connexion" />
+				</form>
+				<p>Pas encore enregistré(e)? <a href='registration.php'>S'enregistrer ici</a></p>
+				<p>Retourner sur le site <a href='index.html'>ici</a></p>
+			</div>
+			<?php
+		} 
+		?>
+	</body>
 </html>
