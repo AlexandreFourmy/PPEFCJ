43d9864896f1eeb85a874c8af5e3caddff65efb6
julien.dussart <dussart.julien.jp@gmail.com>
2019-03-11 09:23:02 +0100

    Creation du client lourd

---
+++ Projet_PPE_Situation_2/JavaApplication2/src/javaapplication2/Bateau.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author julien.dussart
+ */
+public class Bateau {
+    
+    private String idBat;
+    private String nomBat;
+    private double LongueurBat;
+    private double Largeurbat;
+    
+    public Bateau (String unId, String unNom, double uneLongueur, double uneLargeur){
+        
+    }
+    
+}
